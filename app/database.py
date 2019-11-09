e6dc58dbf34a4405191b9106fc5ff25dfd244adf
YHJzw <wind9481@naver.com>
2019-11-09 15:35:47 +0900

    easier db setting alternation via def __init__ grammar fix '!= None' to 'is not None' sql fix for l_pt change in return value

--- app/database.py
+++ app/database.py
@@ -1,7 +1,13 @@
 import pymysql
 
 class Database:
-    # def __init__(self): # db 연결 유지
+    def __init__(self):  # db 설정
+        self.host = 'localhost'
+        self.port = 3306
+        self.user = 'NLP'
+        self.pw = 'haltheta'
+        self.charset = 'utf8'
+        self.db = 'qna_list'
 
     def get_sql(self, sql):  # sql 실행 / 성공 시 딕셔너리 형태로 반환 실패 시 -1
         conn = None
@@ -9,7 +15,7 @@ class Database:
 
         try:
             conn = pymysql.connect(
-                host='localhost', port=3306, user='NLP', password='haltheta', charset='utf8', db='qna_list'
+                host=self.host, port=self.port, user=self.user, password=self.pw, charset=self.charset, db=self.db
             )
 
             with conn.cursor(pymysql.cursors.DictCursor) as curs:
@@ -20,30 +26,22 @@ class Database:
             print(e)
 
         finally:
-            if conn != None:
+            if conn is not None:
                 conn.close()
 
             return rs
 
-    def get_question_list(self):  # 질문 리스트 가져오기 딕셔너리 형태
-        res = self.get_sql('select * from q_list')
-
-        return res
+    def get_question_list(self):  # 질문 리스트 가져오기 딕셔너리 리스트
+        return self.get_sql('select * from q_list')
 
     def question_count(self):  # 질문 갯수 반환
-        res = self.get_sql('select COUNT(question) as count from q_list')
-
-        return res
+        return self.get_sql('select COUNT(question) as count from q_list')[0]['count']
 
     def answer_count(self):  # 답변 갯수 반환
-        res = self.get_sql('select COUNT(id) as count from a_list')
+        return self.get_sql('select COUNT(id) as count from a_list')[0]['count']
 
-        return res
-    
     def answer_max(self):  # 답변 최대값 반환
-        res = self.get_sql('select MAX(id) as max from a_list')
-
-        return res
+        return self.get_sql('select MAX(id) as max from a_list')[0]['max']
 
     def get_answer(self, id):  # 성공 시 답변 / 실패 시 -1
         conn = None
@@ -51,7 +49,7 @@ class Database:
 
         try:
             conn = pymysql.connect(
-                host='localhost', port=3306, user='NLP', password='haltheta', charset='utf8', db='qna_list'
+                host=self.host, port=self.port, user=self.user, password=self.pw, charset=self.charset, db=self.db
             )
 
             with conn.cursor(pymysql.cursors.DictCursor) as curs:
@@ -65,23 +63,23 @@ class Database:
             print(e)
 
         finally:
-            if conn != None:
+            if conn is not None:
                 conn.close()
 
             return rs
 
-    def add_answer(self, a):  # 답변 등록 / 답변 id 반환 실패시 -1
+    def add_answer(self, answer):  # 답변 등록 / 답변 id 반환 실패시 -1
         conn = None
         rs = -1
 
         try:
             conn = pymysql.connect(
-                host='localhost', port=3306, user='NLP', password='haltheta', charset='utf8', db='qna_list'
+                host=self.host, port=self.port, user=self.user, password=self.pw, charset=self.charset, db=self.db
             )
 
             with conn.cursor(pymysql.cursors.DictCursor) as curs:
                 sql = 'insert into a_list (answer) values (%s)'
-                curs.execute(sql, (a))
+                curs.execute(sql, answer)
                 conn.commit()
 
                 rs = curs.lastrowid
@@ -90,23 +88,23 @@ class Database:
             print(e)
 
         finally:
-            if conn != None:
+            if conn is not None:
                 conn.close()
 
             return rs
 
-    def add_question(self, q, a_no):  # 질문 등록 / 성공 시 1 실패 시 -1 반환
+    def add_question(self, question, a_no):  # 질문 등록 / 성공 시 1 실패 시 -1 반환
         conn = None
         rs = -1
 
         try:
             conn = pymysql.connect(
-                host='localhost', port=3306, user='NLP', password='haltheta', charset='utf8', db='qna_list'
+                host=self.host, port=self.port, user=self.user, password=self.pw, charset=self.charset, db=self.db
             )
 
             with conn.cursor(pymysql.cursors.DictCursor) as curs:
                 sql = 'insert into q_list (question, answer_no, l_pt) values (%s, %s, 0)'
-                curs.execute(sql, (q, a_no))
+                curs.execute(sql, (question, a_no))
                 conn.commit()
 
                 rs = 1
@@ -115,35 +113,27 @@ class Database:
             print(e)
 
         finally:
-            if conn != None:
+            if conn is not None:
                 conn.close()
 
             return rs
 
-    def modify_pt(self, q_id, change):
+    def modify_pt(self, q_id, change): # id의 질문의 l_pt(포인트) +- 하기 // 성공시 1, 실패시 -1
         conn = None
         rs = -1
-        pt = 0
 
         try:
             conn = pymysql.connect(
-                host='localhost', port=3306, user='NLP', password='haltheta', charset='utf8', db='qna_list'
+                host=self.host, port=self.port, user=self.user, password=self.pw, charset=self.charset, db=self.db
             )
 
             with conn.cursor(pymysql.cursors.DictCursor) as curs:
-                sql = 'select l_pt from q_list where id = %s'
-                curs.execute(sql, q_id)
-                rs = curs.fetchall()
-
-                pt = rs[q_id - 1]['l_pt']
-
                 if change == -1:
-                    pt -= 1
+                    sql = 'update q_list set l_pt = l_pt - 1 where id = %s'
                 else:
-                    pt += 1
+                    sql = 'update q_list set l_pt = l_pt + 1 where id = %s'
 
-                sql = 'update q_list set l_pt = %s where id = %s'
-                curs.execute(sql, (pt, q_id))
+                curs.execute(sql, q_id)
                 conn.commit()
                 rs = 1
 
@@ -151,38 +141,30 @@ class Database:
             print(e)
 
         finally:
-            if conn != None:
+            if conn is not None:
                 conn.close()
 
             return rs
 
+
 # 예제 나중에 지우기
 '''
-    qna_db = DBFuncs()
-    row = qna_db.get_question_list()
-    if row != -1:
+if __name__ == "__main__":
+    db = Database()
+
+    print("question_count: %s" % db.question_count())
+    print("answer_count: %s" % db.answer_count())
+    print("answer_max: %s" % db.answer_max())
+    print("answer for id 1: %s" % db.get_answer(1))
+    # print("add answer returns id #")
+    # print("add question returns 1 when successful: %s" %db.add_question("DB TEST", db.add_answer("DB TEST")))
+    print("modifies l_pt of question by -1: %s" % db.modify_pt(1, -1))
+
+    row = db.get_question_list()
+    if row is not None or row != -1:
         for i in row:
             print(i['question'])
+    print(row[0])
 
-    count = qna_db.question_count()
-    if count != -1:
-        print(count[0]['count'])
-
-    res = qna_db.add_answer('답변 벡터값에 대한 벡터 분류로 구분합니다.')
-    if res == -1:
-        print('insert error')
-    else:
-        res = qna_db.add_question('유사한 질문은 어떻게 구분하나요?', res)
-
-    res = qna_db.modify_pt(1, -1)
-    if res != -1:
-        row = qna_db.get_question_list()
-        if row != -1:
-            for i in row:
-                print(i['l_pt'])
-
-    res = qna_db.get_answer(2)
-    print(res)
-    
     # curl -i -H "Content-Type: application/json" -X POST -d "{\"question\":\"Rest API \uD14C\uC2A4\uD2B8\",\"answer\":\"REST API \uD14C\uC2A4\uD2B8\"}" http://localhost:5000/plugin/AQA
 '''
