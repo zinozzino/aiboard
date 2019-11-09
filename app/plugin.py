e9156bf79156ac727cc9eb95fe2704f0f9e94007
YHJzw <wind9481@naver.com>
2019-11-09 17:09:47 +0900

    easier db setting alternation via def __init__ grammar fix '!= None' to 'is not None' sql fix for l_pt change in return value

--- app/plugin.py
+++ app/plugin.py
@@ -9,8 +9,9 @@ class API:
         api = Api(self.app)
 
         api.add_resource(Plugin, '/')
-        api.add_resource(RequestAnalysis, '/plugin/RA')
-        api.add_resource(AddQnA, '/plugin/AQA')
+        api.add_resource(Classify, '/plugin/classify')
+        api.add_resource(QnA, '/plugin/qna')
+        api.add_resource(Relearn, '/plugin/relearn')
 
     def run_app(self):
         self.app.run(debug=True)
@@ -22,47 +23,48 @@ class Plugin(Resource):
         return "Plugin For AI QnA Board"
 
 
-class RequestAnalysis(Resource):
-    def __init__(self):
-        self.db = Database();
-
-    def get(self):
-        return "QnA API - Request Analysis"
-
-    def put(self):
-        res = -1
-        analyzed = -1
-
-        try:
-            question = request.get_json().get('question')
-            # print(question)
-
-            if question is not None:
-                analyzed = 1
-                # 자연어 처리 함수 여기에
-
-            if analyzed != -1:
-                answer = self.db.get_answer(analyzed)
-                # print(answer)
-                res = 1
-
-        except Exception as e:
-            print("Error in RA")
-            print(e)
-
-        finally:
-            if res == 1:
-                return jsonify({"answer": answer})
-            else:
-                return jsonify({"answer": res})
-
-
-class AddQnA(Resource):
+class Classify(Resource):
     def __init__(self):
         self.db = Database()
 
     def get(self):
-        return "QnA API - Add QnA"
+        res = -1
+        analyzed = -1
+        question = None
+
+        try:
+            data = request.get_json()
+            if data is not None:
+                question = data.get('question')
+
+            if question is not None:
+                analyzed = 1
+            # 자연어 처리 함수 여기에
+
+            if analyzed != -1:
+                answer = self.db.get_answer(analyzed)
+                res = 1
+
+        except Exception as e:
+            print("Error in classify")
+            print(e)
+
+        finally:
+            if data is None:
+                return "QnA API - Classify"
+            else:
+                if res == 1:
+                    return jsonify({"answer": answer})
+                else:
+                    return jsonify({"answer": res})
+
+
+class QnA(Resource):
+    def __init__(self):
+        self.db = Database()
+
+    def get(self):
+        return "QnA API - QnA"
 
     def post(self):
         res = -1
@@ -82,11 +84,27 @@ class AddQnA(Resource):
                     res = 1
 
         except Exception as e:
-            print("Error in AQA")
+            print("Error in QnA")
             print(e)
 
         finally:
             return jsonify({"result": res})
+
+
+class Relearn(Resource):
+    def get(self):
+        return "QnA API - Relearn"
+
+    def post(self):
+        try:
+            return 1
+
+        except Exception as e:
+            print("Error in Relearn")
+            print(e)
+
+        finally:
+            return 1
 
 
 if __name__ == "__main__":
@@ -94,32 +112,4 @@ if __name__ == "__main__":
     CA.run_app()
 
 # 예제 나중에 지우기
-'''
-    qna_db = DBFuncs()
-    row = qna_db.get_question_list()
-    if row != -1:
-        for i in row:
-            print(i['question'])
-
-    count = qna_db.question_count()
-    if count != -1:
-        print(count[0]['count'])
-
-    res = qna_db.add_answer('답변 벡터값에 대한 벡터 분류로 구분합니다.')
-    if res == -1:
-        print('insert error')
-    else:
-        res = qna_db.add_question('유사한 질문은 어떻게 구분하나요?', res)
-
-    res = qna_db.modify_pt(1, -1)
-    if res != -1:
-        row = qna_db.get_question_list()
-        if row != -1:
-            for i in row:
-                print(i['l_pt'])
-
-    res = qna_db.get_answer(2)
-    print(res)
-    
-    # curl -i -H "Content-Type: application/json" -X POST -d "{\"question\":\"Rest API \uD14C\uC2A4\uD2B8\",\"answer\":\"REST API \uD14C\uC2A4\uD2B8\"}" http://localhost:5000/plugin/AQA
-'''
+# curl -i -H "Content-Type: application/json" -X POST -d "{\"question\":\"Rest API \uD14C\uC2A4\uD2B8\",\"answer\":\"REST API \uD14C\uC2A4\uD2B8\"}" http://localhost:5000/plugin/qna
